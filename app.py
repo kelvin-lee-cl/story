4c577783629b196dc1c4fe79151ea29a863e39fa
kelvin-lee-cl <kelvinlee@futureleadersunion.com>
2025-04-25 09:28:48 +0800

    new_py

--- app.py
+++ app.py
@@ -42,4 +42,5 @@ def generate_story():
     return jsonify({'story': response.generations[0].text})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
